51192540e660613360031036e2b80d4fab80121b
Rajan <rajanjaiswal0824@gmail.com>
2023-09-30 21:45:28 +0530

    First java Program

---
+++ FirstProgram.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class FirstProgram{
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome to Java");
+        System.out.println("SYstem date");
+        System.out.println(new Date());
+    }
+}
